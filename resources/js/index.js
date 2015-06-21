0e97b284375ed1f1b9ea010cfb7c0a94b3b1553a
Ewout Merckx <ewmerckx.merckx@ugent.be>
2015-06-21 20:44:13 +0200

    The rectangles in all colors are being drawn

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,6 +1,37 @@
 $(document).ready ( function() {
-    alert('ok');
+    var $file = "resources/data/colors.json";
+
+    // get the colors
+    var $getColors = $.getJSON( $file, function($data) {
+
+        // Get the colors from the JSON file
+        var $colors = [];
+        $.each( $data, function( $key, $val ){
+            //console.log($val);
+            $.each( $val[0].codes, function($key2, $val2){
+                $colors.push($val2)
+            });
 
 
+        });
 
+        // Draw the rectangles
+        var $canvas_width = $( "#canvas" ).width();
+        var $shape_width = 100;
+        var $amount_colors = $colors.length;
+        var $amount_rectangles = $canvas_width/$shape_width;
+
+        for(var $i=0; $i<$amount_rectangles; $i++) {
+            var $color_number = $i % $amount_colors;
+            var $id = "color" + $color_number;
+            var $new_section = "<section class=\"shape " + $id + "\"></section";
+            $("#canvas").append($new_section);
+
+            $("." + $id).css({
+                "left": $i,
+                "width": $shape_width,
+                "background-color": $colors[$color_number]
+            });
+        }
+    });
 });
